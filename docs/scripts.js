3540cc833766f467472e5c0c256418cfd39a22ea
ekersey <ekersey@users.noreply.github.com>
2024-02-01 00:02:19 -0500

    track key down duration

--- docs/scripts.js
+++ docs/scripts.js
@@ -4,11 +4,23 @@ const log = document.getElementById("log");
 input.addEventListener("keyup", logKeyUp);
 input.addEventListener("keydown", logKeyDown);
 
+var startTimes = {};
+
 function logKeyUp(e) {
-  log.textContent += e.code + " UP; " + " key: '" + e.key + "', ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + "\r\n";
+	var elapsed = null;
+	if (startTimes[e.code]) {
+		elapsed = new Date().getTime() - startTimes[e.code];
+		startTimes[e.code] = null;
+	}
+  log.textContent += e.code + " UP; " + " key: '" + e.key + "', ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + (elapsed ? ", elapsed: " + elapsed + "ms" : "") + "\r\n";
 }
 
 function logKeyDown(e) {
+  if (!e.repeat) {
+	  if (!startTimes[e.code]) {
+		  startTimes[e.code] = new Date().getTime();
+	  }
+  }
   log.textContent += e.code + " DOWN; " + " key: '" + e.key + "', ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + "\r\n";
 }
 
