8d1e9636d04778d25b169f73c4c85206239b7c2d
ekersey <ekersey@users.noreply.github.com>
2024-01-31 23:08:14 -0500

    fixed

--- docs/scripts.js
+++ docs/scripts.js
@@ -5,9 +5,13 @@ input.addEventListener("keyup", logKeyUp);
 input.addEventListener("keydown", logKeyDown);
 
 function logKeyUp(e) {
-  log.textContent += e.code + " UP; " + " key: " + e.key + ", ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + "\r\n";
+  log.textContent += e.code + " UP; " + " key: '" + e.key + "', ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + "\r\n";
 }
 
 function logKeyDown(e) {
-  log.textContent += e.code + " DOWN; " + " key: " + e.key + ", ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + "\r\n";
+  log.textContent += e.code + " DOWN; " + " key: '" + e.key + "', ctrl: " + e.ctrlKey + ", alt: " + e.altKey + ", shift: " + e.shiftKey + ", meta: " + e.metaKey + ", repeat: " + e.repeat + ", keyCode: " + e.keyCode + ", charCode: " + e.charCode + ", which: " + e.which + "\r\n";
+}
+
+function eraseText() {
+    location.reload()
 }
